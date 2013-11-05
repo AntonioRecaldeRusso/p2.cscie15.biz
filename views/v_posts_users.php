40ec5613dc72214d60a6e738b5154e5ed8fa5548
asmallorange <jeanalesi007@gmail.com>
2013-11-05 13:49:23 -0300

    added hyperlinks to profiles

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,4 +1,5 @@
 <div id="wrapper">
+    <h4 id="table_title">Click the Users to visit their profiles or toggle Follow-Unfollow!</h4>
 
     <div class="table" >
                 <table >
@@ -6,21 +7,21 @@
 
     <?php foreach($users as $user): ?>
                     <tr>
-                        <td class="first_name">
+                        <td class="name">
 
     <!-- Print this user's name -->
-    <?php echo $user['first_name']?> <?php echo $user['last_name']?>
+    <a href=<?php echo "'/users/profile/".$user['username']."'" ?>><?php echo $user['first_name']?> <?php echo $user['last_name']?></a>
 
                         </td>
                         <td class="link">
 
     <!-- If there exists a connection with this user, show a unfollow link -->
     <?php if(isset($connections[$user['user_id']])): ?>
-        <a href='/posts/unfollow/<?php echo $user['user_id']?>'>Unfollow</a>
+        <a class="unfollow" href='/posts/unfollow/<?php echo $user['user_id']?>'>Unfollow</a>
 
     <!-- Otherwise, show the follow link -->
     <?php else: ?>
-        <a href='/posts/follow/<?php echo $user['user_id']?>'>Follow</a>
+        <a class="follow" href='/posts/follow/<?php echo $user['user_id']?>'>Follow</a>
     <?php endif; ?>
                     
                         </td>
